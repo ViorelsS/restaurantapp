a26aee689275728fe1b657f3238ba5c0cca69674
ViorelS <viorelss@icloud.com>
2022-09-11 23:47:23 +0200

    Project initialized

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className="fixed z-50 w-screen bg-slate-300 p-6">
+            <div className="w-[50px] h-[50px] bg-red-500">Coap</div> Header
+        </div>
+    );
+};
+
+export default Header;
